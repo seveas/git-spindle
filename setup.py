03db497e0dd0bd25455ba19f04dc658008980b9e
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2013-06-29 13:34:16 +0200

    Use iter_user_repos where appropriate

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='hub',
-    version="1.11",
+    version="1.12",
     description='Github integration for git',
     author='Dennis Kaarsemaker',
     author_email='dennis@kaarsemaker.net',
