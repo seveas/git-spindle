9fc06f2b849a30c2996db8c1a1575857863480cc
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2014-05-28 22:07:11 +0200

    Version 1.21

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='hub',
-    version="1.20",
+    version="1.21",
     description='Github integration for git',
     author='Dennis Kaarsemaker',
     author_email='dennis@kaarsemaker.net',
