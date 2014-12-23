a49cb5e2c11efa7a43e676ac4ebe2d35ad32de91
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2014-12-23 13:54:56 +0100

    Version 2.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='git-spindle',
-    version="2.1",
+    version="2.2",
     description='Git subcommands for integrating with central services like github, gitlab and bitbucket',
     author='Dennis Kaarsemaker',
     author_email='dennis@kaarsemaker.net',
