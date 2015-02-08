95345ae9726a848ebe455bb454b8138f9ee9fe08
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2015-02-08 21:23:57 +0100

    Mangle generated manpages to handle ansi escape codes

--- docs/ansicolor.py
+++ docs/ansicolor.py
@@ -24,24 +24,22 @@ codes = {
     47: {"background_color": "white"},
 }
 
-def process_escape(match):
+def process_escape_html(match):
     items = [int(x) for x in match.group(1).split(';')]
     if 0 in items:
         return "</span>"
-    else:
-        attrs = [codes[x] for x in codes if x in items]
-        if 38 in items:
-            color = items[items.index(38)+2]
-            color = {
-                22:  "005f00",
-                28:  "008700",
-                64:  "5f8700",
-                65:  "5f875f",
-                237: "3a3a3a",
-            }[color]
-            return '<span style="color: #%s">' % color
-        return '<span style="%s">' % '; '.join([", ".join([': '.join(x) for x in attr.items()]) for attr in attrs])
-    print(match.groups())
+    attrs = [codes[x] for x in codes if x in items]
+    if 38 in items:
+        color = items[items.index(38)+2]
+        color = {
+            22:  "005f00",
+            28:  "008700",
+            64:  "5f8700",
+            65:  "5f875f",
+            237: "3a3a3a",
+        }[color]
+        return '<span style="color: #%s">' % color
+    return '<span style="%s">' % '; '.join([", ".join([': '.join(x) for x in attr.items()]) for attr in attrs])
 
 def mangle_html(app, exception):
     if app.builder.name != 'html' or exception:
@@ -55,5 +53,28 @@ def mangle_html(app, exception):
                 fd.truncate()
                 fd.write(data2)
 
+def process_escape_man(match):
+    items = [int(x) for x in match.group(1).split(';')]
+    if 0 in items:
+        return "\\fP"
+    if 1 in items:
+        return '\\fB'
+    if 4 in items:
+        return '\\fI'
+    return ''
+
+def mangle_man(app, exception):
+    if app.builder.name != 'man' or exception:
+        return
+    for file in glob.glob(os.path.join(app.builder.outdir, '*.1')):
+        with open(file, 'r+') as fd:
+            data = fd.read()
+            data2 = re.sub(escape_code_re, process_escape_man, data)
+            if data2 != data:
+                fd.seek(0, os.SEEK_SET)
+                fd.truncate()
+                fd.write(data2)
+
 def setup(app):
     app.connect('build-finished', mangle_html)
+    app.connect('build-finished', mangle_man)
