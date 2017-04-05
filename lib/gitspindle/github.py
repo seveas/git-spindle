a431657a8b164109d48c3d1d49778a673aaf2386
Björn Kautler <Bjoern@Kautler.net>
2017-04-05 09:40:08 +0200

    fix hook editing
    When editing a hook you got a complaint that the argument to
    'properties/config' is not an object and that the argument to
    'properties/events' is not an array. The name as first parameter was
    plainly wrong and thus the parameters were shifted by one and their types
    not like expected.

--- lib/gitspindle/github.py
+++ lib/gitspindle/github.py
@@ -644,7 +644,7 @@ class GitHub(GitSpindle):
         events = settings.pop('events', ','.join(hook.events)).split(',')
         config = hook.config
         config.update(settings)
-        hook.edit(opts['<name>'], config, events)
+        hook.edit(config, events)
 
     @command
     def fetch(self, opts):
