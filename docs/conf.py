0b9509c91101eb65b40bbd50e12c20b615c252db
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2018-04-15 19:13:55 +0200

    Switch docs to the rtd theme

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,5 @@
-import cloud_sptheme as csp
 import os, sys
+import sphinx_rtd_theme as theme
 sys.path.insert(0, os.path.dirname(__file__))
 source_suffix = '.rst'
 master_doc = 'index'
@@ -9,13 +9,11 @@ version = '3.4'
 release = '3.4.3'
 exclude_patterns = ['_build']
 pygments_style = 'sphinx'
-html_theme = 'cloud'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 html_theme_options = {
-    'roottarget': 'index',
-    'stickysidebar': False,
 }
-html_theme_path = [csp.get_theme_dir()]
+html_theme_path = [theme.get_html_theme_path()]
 html_show_sourcelink = False
 html_show_sphinx = False
 extensions = ['ansicolor']
