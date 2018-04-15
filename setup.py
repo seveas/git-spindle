3e219fe645531ebc9e87365b10438f9eed0d81f4
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2018-04-15 19:28:12 +0200

    Silly version bump to help CI

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='git-spindle',
-    version="3.4.3",
+    version="3.4.4",
     description='Git subcommands for integrating with central services like github, gitlab and bitbucket',
     author='Dennis Kaarsemaker',
     author_email='dennis@kaarsemaker.net',
