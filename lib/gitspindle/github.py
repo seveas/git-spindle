321a8e8010696dbdfa7b42cb5a24f4a7132e7544
Abdo Roig-Maranges <abdo.roig@gmail.com>
2018-04-17 19:12:20 +0200

    github: simplify condition

--- lib/gitspindle/github.py
+++ lib/gitspindle/github.py
@@ -1114,7 +1114,7 @@ be ignored, the first line will be used as title for the issue.""" % (repo.owner
         else:
             keys = self.gh.user(user).keys()
         for key in keys:
-            if hasattr(key, 'title') and key.title:
+            if getattr(key, 'title', None):
                 print("%s %s" % (key.key, key.title or ''))
             else:
                 print("%s" % key.key)
@@ -1538,7 +1538,7 @@ will be ignored""" % (name, tag)
             for pkey in keys:
                 algo, key = pkey.key.split()[:2]
                 algo = algo[4:].upper()
-                if hasattr(pkey, 'title') and pkey.title:
+                if getattr(pkey, 'title', None):
                     print("%s key%s...%s (%s)" % (algo, ' ' * (6 - len(algo)), key[-10:], pkey.title))
                 else:
                     print("%s key%s...%s" % (algo, ' ' * (6 - len(algo)), key[-10:]))
